c86063b9f2967d81495c5707ce571bb9bed10f02
meetwith2p <your.email@example.com>
2023-12-05 22:12:19 +0530

    [REM] removed unnecessary imports.

--- item/urls.py
+++ item/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from django.contrib.auth.decorators import login_required
-from django.views.generic import TemplateView
 from item import views
 
 
@@ -9,5 +7,4 @@ app_name = 'item'
 
 urlpatterns = [
       path('home/', views.ItemView.as_view(), name='home'),
-      path('summary/', login_required(TemplateView.as_view(template_name='summary_page.html')), name='summary'),
 ]
