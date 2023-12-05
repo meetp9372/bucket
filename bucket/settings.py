17878af68a51b7356f7768dc51b7c16e7b8f3e38
meetwith2p <your.email@example.com>
2023-12-05 21:34:32 +0530

    [ADD] added allowed hosts.

--- bucket/settings.py
+++ bucket/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-^ik=$kxp9p40(jy*r_q^vn-6eku9r0(a8ukmxls-9wptf#nsvv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
