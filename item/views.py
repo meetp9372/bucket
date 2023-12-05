ba99334d805b752cc6c6347e1ce1b7db5adb1e94
meetwith2p <your.email@example.com>
2023-12-05 22:13:06 +0530

    [ADD] added login required and Item creation.

--- item/views.py
+++ item/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render
 from django.views import View
+from item.models import Item
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 
 class ItemView(View):
     template_name = 'home.html'
 
+    @method_decorator(login_required)
     def get(self, request):
         return render(request, self.template_name)
 
+    @method_decorator(login_required)
     def post(self, request):
 
         total_cost = 0
         for item_number in range(1, int(request.POST["item_length"])+1):
-            if request.POST[f"price_{item_number}"]:
+            item_name = request.POST[f"item_{item_number}"]
+            item_price = request.POST[f"price_{item_number}"]
+            if item_name and item_price:
+                Item.objects.create(name=item_name, price=item_price)
                 total_cost += float(request.POST[f"price_{item_number}"])
 
         return render(request, 'summary_page.html', context={'items_total_cost': round(total_cost, 3)})
